a79bb80a4cb7219556740a54bccb258739549289
Anna Solovykh <ms.teachy.solo@gmail.com>
2024-06-28 19:36:54 -0400

    fix: refactor dashboard structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { EventProvider } from './components/Dashboard/Calendar/EventContext';
-import KFCalendar from './components/Dashboard/Calendar/KFCalendar';
+import KFCalendar from './components/Dashboard/Calendar/Calendar/KFCalendar';
 
 import HomeDashboard from './components/Dashboard/HomeDashboard/HomeDashboard';
 import LandingLayout from './components/shared/LandingLayout';
+import { EventProvider } from './context/EventContext';
 import Activate from './pages/Activate/Activate';
 import Dashboard from './pages/Dashboard/Dashboard';
 
