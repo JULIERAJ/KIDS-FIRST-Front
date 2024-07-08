240f88c46efbdcaa8b76342cd74ec40225e1a86f
Nandini Asokan <nandini.asokan92@gmail.com>
2024-07-08 13:41:31 -0400

    feat(button): Added custom NavButton and icon/text styling

--- src/components/shared/ui/Button/CustomButton.jsx
+++ src/components/shared/ui/Button/CustomButton.jsx
@@ -2,16 +2,33 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 
+import { NavLink } from 'react-router-dom';
+
 import './buttonStyles.css';
 import './buttonSizes.css';
-import './textStyles.css';
+import './contentStyles.css';
 
 //TODO: Handle event handlers on button. See Activate.
 //TODO2: Unit test button on Activate page
-export const CustomButton = ({ type, styles, link, size, variant, children }) => {
+export const CustomButton = ({
+  styles,
+  link,
+  iconLeft,
+  iconRight,
+  iconLeftStyles,
+  iconRightStyles,
+  children,
+  ...props
+}) => {
   return (
-    <Button type={type} className={classNames(styles)} href={link} size={size} variant={variant}>
-      {children}
+    <Button className={classNames(styles)} href={link} {...props}>
+      {iconLeft && (
+        <img src={iconLeft} className={classNames('icon_left', iconLeftStyles)} />
+      )}
+      <span className='text'>{children}</span>
+      {iconRight && (
+        <img src={iconRight} className={classNames('icon_right', iconRightStyles)} />
+      )}
     </Button>
   );
 };
@@ -19,8 +36,46 @@ export const CustomButton = ({ type, styles, link, size, variant, children }) =>
 CustomButton.propTypes = {
   type: PropTypes.string,
   styles: PropTypes.string,
+  iconLeft: PropTypes.string,
+  iconLeftStyles: PropTypes.string,
+  iconRight: PropTypes.string,
+  iconRightStyles: PropTypes.string,
   link: PropTypes.string,
-  size: PropTypes.string,
-  variant: PropTypes.string,
+  children: PropTypes.any,
+};
+
+export const NavButton = ({
+  styles,
+  link,
+  iconLeft,
+  iconRight,
+  iconLeftStyles,
+  iconRightStyles,
+  children,
+  ...props
+}) => {
+  return (
+    <NavLink to={link}>
+      <Button className={classNames(styles)} {...props}>
+        {iconLeft && (
+          <img src={iconLeft} className={classNames('icon_left', iconLeftStyles)} />
+        )}
+        <span className='text'>{children}</span>
+        {iconRight && (
+          <img src={iconRight} className={classNames('icon_right', iconRightStyles)} />
+        )}
+      </Button>
+    </NavLink>
+  );
+};
+
+NavButton.propTypes = {
+  type: PropTypes.string,
+  styles: PropTypes.string,
+  iconLeft: PropTypes.string,
+  iconLeftStyles: PropTypes.string,
+  iconRight: PropTypes.string,
+  iconRightStyles: PropTypes.string,
+  link: PropTypes.string,
   children: PropTypes.any,
 };
