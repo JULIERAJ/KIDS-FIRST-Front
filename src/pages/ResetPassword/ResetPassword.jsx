ded14ccce29eacb3f380d63d5e8165bacfc43a34
Nandini Asokan <nandini.asokan92@gmail.com>
2024-07-20 14:34:39 -0400

    fix(change-password-page): First draft of updated layout and password input to match figma design

--- src/pages/ResetPassword/ResetPassword.jsx
+++ src/pages/ResetPassword/ResetPassword.jsx
@@ -1,33 +1,38 @@
 import { useCallback, useEffect, useState } from 'react';
 import { Container, Form } from 'react-bootstrap';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 import { resetPassword, resetPasswordLink } from '@api';
 
-import MessageBar from '@components/MessageBar';
+// import MessageBar from '@components/MessageBar';
 
-import FatherSonBlock from '@components/shared/FatherSonBlock';
-import Header from '@components/shared/Header';
+//import FatherSonBlock from '@components/shared/FatherSonBlock';
+//import Header from '@components/shared/Header';
 import { CustomButton } from '@components/shared/ui/Button/CustomButton';
 import FormPasswordInput from '@components/shared/ui/form/FormPasswordInput';
-import TextLink from '@components/shared/ui/TextLink';
-import logoPswdChanged from '@media/icons/pswd-changed.png';
+//import TextLink from '@components/shared/ui/TextLink';
+// import logoPswdChanged from '@media/icons/pswd-changed.png';
 
 import styles from './ResetPassword.module.css';
 
-const DEFAULT_ERROR_MESSAGE =
-  'You are using symbols in your passwords or your passwords do not match.';
+const DEFAULT_MESSAGE =
+  'Include at least: • 8 characters  • upper and lower case characters  • a number  • a special character';
 
 export default function ResetPassword() {
   const { email, resetPasswordToken } = useParams();
   const [userValid, setUserValid] = useState(null);
   const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [defaultMessage, setDefaultMessage] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const [sentEmail, setSentEmail] = useState(false);
-  const [success, setSuccess] = useState(false);
 
-  const checkValid = useCallback(async () => {
+  useEffect(() => {
+    verifyEmail();
+  }, [email, resetPasswordToken]);
+
+  const verifyEmail = async () => {
     try {
       const { data } = await resetPasswordLink(email, resetPasswordToken);
       setUserValid(data);
@@ -35,23 +40,23 @@ export default function ResetPassword() {
       //eslint-disable-next-line no-console
       console.error(err);
     }
-  }, [email, resetPasswordToken]);
-  useEffect(() => {
-    checkValid();
-  }, [checkValid, email, resetPasswordToken]);
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
 
   const handleChangePassword = useCallback(
     async (e) => {
       e.preventDefault();
-      setErrorMessage('');
-      if (password === '' || confirmPassword === '') {
-        setErrorMessage('Please fill out both password fields.');
+
+      if (password === '') {
+        setErrorMessage(DEFAULT_MESSAGE);
         return;
+      } else {
+        setErrorMessage('');
       }
-      if (password !== confirmPassword) {
-        setErrorMessage(DEFAULT_ERROR_MESSAGE);
-        return;
-      }
+
       try {
         const { data } = await resetPassword(
           email,
@@ -59,76 +64,52 @@ export default function ResetPassword() {
           resetPasswordToken
         );
         setUserValid(data);
-        setSuccess(true);
+        setSuccessMessage('Password accepted');
         setSentEmail(true);
       } catch (err) {
-        setErrorMessage(DEFAULT_ERROR_MESSAGE);
+        setErrorMessage(DEFAULT_MESSAGE);
       }
     },
-    [email, password, confirmPassword, resetPasswordToken]
+    [email, password, resetPasswordToken]
   );
 
   return (
-    <>
-      <Header
-        widget={
-          <TextLink title='Already a member?' to='/signin' linkTitle='Log in' />
-        }
-      />
-      <Container className='content-layout py-4'>
-        <FatherSonBlock>
-          <h1 className={styles.title}>Change Password</h1>
-          {!success && errorMessage && (
-            <MessageBar variant='error'>{errorMessage}</MessageBar>
-          )}
-          {success && sentEmail && (
-            <>
-              <div className={styles['success-password']}>
-                <img
-                  className={styles.loadingLogo}
-                  src={logoPswdChanged}
-                  alt='password-changed-successfully'
-                />
-                <span>Password has been changed!</span> <br />
-                <span>
-                  Your password has been changed successfully.
-                  <br /> Please login with the new password.
-                </span>
-                <br />
-                <Link to='/signin' className={`btn ${styles['back-login']}`}>
-                  Back to Log In
-                </Link>
-              </div>
-            </>
-          )}
+    <div className={styles.wrapper}>
+      <Container className={styles.container}>
+        <h1 className={styles.title}>Set a New Password</h1>
+        <p className={styles.description}>
+          Please create a new password, making sure it differs from any previous
+          passwords you have used
+        </p>
 
-          {userValid && !sentEmail && (
-            <Form className='py-4' onSubmit={handleChangePassword} noValidate>
-              <FormPasswordInput
-                required
-                value={password}
-                label='New Password'
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <FormPasswordInput
-                id='confirmPassword'
-                label='Password Confirmation'
-                name='confirmPassword'
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                required
-              />
-              <CustomButton
-                styles={`primary-btn w-100 my-3 ${styles.customButton}`}
-                type='submit'
-                size='lg'
-                variant='light'>
-                Change Password
-              </CustomButton>
-            </Form>
-          )}
-        </FatherSonBlock>
+        {userValid && !sentEmail && (
+          <Form onSubmit={handleChangePassword} noValidate>
+            <FormPasswordInput
+              required
+              value={password}
+              label='New Password'
+              labelClassName={styles.label}
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete='off'
+              errors={errorMessage}
+              successMessage={successMessage}
+              showTextPassword={defaultMessage}
+              onFocus={() => !errorMessage && !successMessage && setDefaultMessage(DEFAULT_MESSAGE)}
+              onBlur={() => setDefaultMessage(null)}
+              style={{ paddingBottom: '0px' }}
+              togglePasswordVisibility={togglePasswordVisibility}
+              type={showPassword}
+            />
+            <CustomButton
+              styles='primary-light'
+              style={{ marginTop: '40px', width: '442px', height: '56px' }}
+              type='submit'
+            >
+              Change Password
+            </CustomButton>
+          </Form>
+        )}
       </Container>
-    </>
+    </div>
   );
 }
