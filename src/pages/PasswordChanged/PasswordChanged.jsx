f037c7f6f0add57ebe8ffe3de8d9fbfe5bc87f0a
Nandini Asokan <nandini.asokan92@gmail.com>
2024-07-23 20:00:26 -0400

    fix(password-changed): Updated new password-changed route to display confirmation message

--- src/pages/PasswordChanged/PasswordChanged.jsx
+++ src/pages/PasswordChanged/PasswordChanged.jsx
@@ -1,4 +1,7 @@
+import { Container } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+
+import Header from '@components/shared/Header';
 
 import logoPswdChanged from '@media/icons/pswd-changed.svg';
 
@@ -6,22 +9,25 @@ import styles from './PasswordChanged.module.css';
 
 export default function PasswordChanged() {
   return (
-    <div className={styles.page}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Password Changed</h1>
-        <img
-          className={styles.loadingLogo}
-          src={logoPswdChanged}
-          alt='password-changed-successfully'
-        />
-        <h2 className={styles.subTitle}>Password has been updated!</h2>
-        <p className={styles.description}>
-          You can now log in with the new password.{' '}
-        </p>
-        <NavLink className={styles.link} to='/signin'>
-          Back To Log In
-        </NavLink>
-      </div>
-    </div>
+    <Container className={styles.page}>
+      <Header />
+      <Container className={styles['page-window']}>
+        <Container className={`${styles['page-wrapper']}`}>
+          <h1 className={styles.title}>Password Changed</h1>
+          <img
+            className={styles.loadingLogo}
+            src={logoPswdChanged}
+            alt='password-changed-successfully'
+          />
+          <h2 className={styles.subTitle}>Password has been updated!</h2>
+          <p className={styles.description}>
+            You can now log in with the new password.{' '}
+          </p>
+          <NavLink className={styles.link} to='/signin'>
+            Back To Log In
+          </NavLink>
+        </Container>
+      </Container>
+    </Container>
   );
 }
