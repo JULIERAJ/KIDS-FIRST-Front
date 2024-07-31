67ea89985a2fedca0ba9e4c134578669a2ee9e04
Ekaterina Egorova <61324163+EkaterinaEg@users.noreply.github.com>
2024-07-31 14:02:24 -0400

    refactor(NotificationPage): adapt code to use in verification email page

--- src/components/shared/NotificationPage/NotificationPage.jsx
+++ src/components/shared/NotificationPage/NotificationPage.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import { useNavigate , NavLink } from 'react-router-dom';
+import { useNavigate, NavLink } from 'react-router-dom';
 
 import Header from '@components/shared/Header';
 
@@ -10,29 +10,54 @@ import { CustomButton } from '../ui/Button/CustomButton';
 
 import styles from './NotificationPage.module.css';
 
-export default function NotificationPage({ title, image, altText, message, description, linkText, linkTo, isButton }) {
+export default function NotificationPage({ title,
+  image,
+  altText,
+  message,
+  description,
+  text,
+  linkText,
+  linkTo,
+  isButton,
+  isLoading,
+  emailResent,
+  handleResendEmail }) {
+
   const navigate = useNavigate();
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    if (!isLoading && !emailResent) {
+      handleResendEmail(event);
+      navigate(linkTo);
+    }
+  };
 
   return (
     <Container className={styles.page}>
       <Header />
       <Container className={styles['page-window']}>
-        <Container className={`${styles['page-wrapper']}`}>
-          <h1 className={styles.title}>{title}</h1>
-          {image && <img className={styles.loadingLogo} src={image} alt={altText} />}
-          <h2 className={styles.subtitle}>{message}</h2>
+        <Container className={styles['page-wrapper']}>
+          <h2 className={styles.title}>{title}</h2>
+          {image && <img className={styles['loading-logo']} src={image} alt={altText} />}
+          <h3 className={styles.subtitle}>{message}</h3>
           <p className={styles.description}>{description}</p>
+          <p className={styles.text}>{text}</p>
           {isButton ? (
-            <div className={styles['custom-button-container']}>
-              <CustomButton
-                className={`primary-light ${styles['custom-button']}`}
-                size="med"
-                variant="light"
-                onClick={() => navigate(linkTo)}
-              >
-                {linkText}
-              </CustomButton>
-            </div>
+            !emailResent ? (
+              <div className={styles['custom-button-container']}>
+                <CustomButton
+                  className={`primary-light ${styles['custom-button']}`}
+                  size="med"
+                  variant="light"
+                  onClick={handleClick}
+                  disabled={isLoading}
+                >
+                  {isLoading ? 'Resending...' : linkText}
+
+                </CustomButton>
+              </div>
+            ) : (<p>Email already resent</p>)
           ) : (
             <NavLink className={styles.link} to={linkTo}>
               {linkText}
@@ -50,7 +75,11 @@ NotificationPage.propTypes = {
   altText: PropTypes.string,
   message: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
   linkText: PropTypes.string.isRequired,
   linkTo: PropTypes.string.isRequired,
-  isButton: PropTypes.bool
+  isButton: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  emailResent: PropTypes.bool,
+  handleResendEmail: PropTypes.func,
 };
