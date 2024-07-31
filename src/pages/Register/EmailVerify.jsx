55e9cd0d0958f4ca848870ce1a2f348fb293b2ad
Ekaterina Egorova <61324163+EkaterinaEg@users.noreply.github.com>
2024-07-31 13:58:17 -0400

    refactor: add NotificationPage

--- src/pages/Register/EmailVerify.jsx
+++ src/pages/Register/EmailVerify.jsx
@@ -3,11 +3,9 @@ import React, { useState } from 'react';
 
 import { resendEmailVerification } from '@api';
 
-import FeedbackBlock from '@components/shared/Feedback/FeedbackBlock';
+import NotificationPage from '@components/shared/NotificationPage';
 
-import envelopeImg from '@media/icons/email_sent.png';
-
-import styles from './EmailVerify.module.css';
+import envelopeImg from '@media/icons/email-image.svg';
 
 const EmailVerify = ({ userData }) => {
   const email = userData.email;
@@ -36,26 +34,38 @@ const EmailVerify = ({ userData }) => {
     }
   };
   return (
-    <>
-      <FeedbackBlock message={message} image={envelopeImg}/>
-      <div className={styles.verifyText}>
-        <p>A verification email has been sent to {email}.</p>
-        <p>Please verify your email address to log in to KIDS FIRST.</p>
-      
-      </div>
-      <div className={styles.resendEmailLink}>
-        {/* Disable the link and show loading indicator if isLoading is true or email has already been resent*/}
-        {!emailResent && (
-        /* eslint-disable-next-line */
-          <a href='' onClick={handleResendEmail} disabled={isLoading}>
-            {isLoading ? 'Resending...' : 'Resend Email'}
-          </a>
-        )}
-        {emailResent && (
-          <p>Email already resent</p>
-        )}
-      </div>
-    </>
+    <NotificationPage title='Sign up Kids First'
+      image={envelopeImg}
+      altText="Envelope icon"
+      message={message}
+      description={`An email with a verification link was sent to ${email}. Please verify your email address.`}
+      text="If you haven't received the email within 2 minutes, you can resend the verification link one more time."
+      linkText="Resend verification link"
+      linkTo="#" isButton={true}
+      isLoading={isLoading}
+      emailResent={emailResent}
+      handleResendEmail={handleResendEmail}>
+    </NotificationPage>
+    // <>
+    //   <FeedbackBlock message={message} image={envelopeImg}/>
+    //   <div className={styles.verifyText}>
+    //     <p>A verification email has been sent to {email}.</p>
+    //     <p>Please verify your email address to log in to KIDS FIRST.</p>
+
+  //   </div>
+  //   <div className={styles.resendEmailLink}>
+  //     {/* Disable the link and show loading indicator if isLoading is true or email has already been resent*/}
+  //     {!emailResent && (
+  //     /* eslint-disable-next-line */
+  //       <a href='' onClick={handleResendEmail} disabled={isLoading}>
+  //         {isLoading ? 'Resending...' : 'Resend Email'}
+  //       </a>
+  //     )}
+  //     {emailResent && (
+  //       <p>Email already resent</p>
+  //     )}
+  //   </div>
+  // </>
   );
 };
 
