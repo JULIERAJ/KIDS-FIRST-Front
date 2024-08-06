97130af0b27c724b2c1f5bdb74be6102a00927a6
Nandini Asokan <nandini.asokan92@gmail.com>
2024-08-05 21:58:49 -0400

    fix(sidebar): Simplifying sidebar items by using use NavLink and adding routes to router

--- src/components/Dashboard/Sidebar/SidebarItemsCard.jsx
+++ src/components/Dashboard/Sidebar/SidebarItemsCard.jsx
@@ -1,54 +1,17 @@
 import PropTypes from 'prop-types';
-import React, { useEffect, useState } from 'react';
-import { Nav } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import styles from './Sidebar.module.css';
 
-const SidebarItemsCard = ({
-  title,
-  icon,
-  activeIcon,
-  hoverIcon,
-  path,
-  isActive,
-  onClick,
-}) => {
-  const [isHovered, setIsHovered] = useState(false);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (isActive) {
-      navigate(path);
-    }
-  }, [isActive, path, navigate]);
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    onClick(title, path);
-  };
-
-  const handleKeyDown = (e) => {
-    if (e.key === 'Enter' || e.key === ' ') {
-      e.preventDefault();
-      handleClick(e);
-    }
-  };
-
+const SidebarItemsCard = ({ title, icon, alt, path }) => {
   return (
-    <Nav.Item
-      className={`${styles.nav} ${isActive ? styles.active : ''}`}
-      id={window.location.pathname === path ? 'active' : ''}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      onClick={handleClick}
-      role="button"
-      tabIndex={0}
-      onKeyDown={handleKeyDown}
-    >
-      {isActive ? activeIcon : isHovered ? hoverIcon : icon}
-      <span className={styles.sidebarMenuItem}>{title}</span>
-    </Nav.Item>
+    <NavLink to={path} className={styles.nav}>
+      <span alt={alt} className={'material-symbols-rounded ' + styles.icon}>
+        {icon}
+      </span>
+      <span>{title}</span>
+    </NavLink>
   );
 };
 
--- src/components/Dashboard/Sidebar/sidebarData.js
+++ src/components/Dashboard/Sidebar/sidebarData.js
@@ -2,164 +2,49 @@
 export const SIDEBAR_DATA = [
   {
     title: 'Home',
-    icon:
-      (<span className="material-symbols-rounded " style={{ 'fontSize': '32px' }}>
-        home
-      </span>),
-    activeIcon: (
-      <span className="material-symbols-rounded "
-        style={{
-          'fontSize': '32px',
-          fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40',
-          color: '#DB5C00'
-        }}
-      >home</span>
-    ),
-    hoverIcon:
-      <span className="material-symbols-rounded "
-        style={{
-          'fontSize': '32px',
-          fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40'
-        }}>home</span>,
+    icon: 'home',
     path: '/dashboard/homedashboard',
     alt: 'home',
   },
   {
     title: 'Calendar',
-    icon: <span className="material-symbols-rounded "
-      style={{ 'fontSize': '32px' }}>calendar_month</span>,
-    activeIcon: <span className="material-symbols-rounded "
-      style={{
-        'fontSize': '32px',
-        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40',
-        color: '#DB5C00'
-      }}
-    >calendar_month</span>,
-    hoverIcon:
-      <span className="material-symbols-rounded"
-        style={{
-          'fontSize': '32px',
-          fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40'
-        }}>calendar_month</span>,
+    icon: 'calendar_month',
     path: '/dashboard/calendarview',
     alt: 'calendar',
   },
   {
     title: 'Messages',
-    icon: <span className="material-symbols-rounded "
-      style={{ 'fontSize': '32px' }}>chat</span>,
-    activeIcon: <span className="material-symbols-rounded"
-      style={{
-        'fontSize': '32px',
-        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40',
-        color: '#DB5C00'
-      }}
-    >chat</span>,
-    hoverIcon:
-      <span className="material-symbols-rounded"
-        style={{
-          'fontSize': '32px',
-          fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40'
-        }}>chat</span>,
+    icon: 'chat',
     path: '/dashboard/messages',
     alt: 'messages',
   },
   {
     title: 'Album',
-    icon: <span className="material-symbols-rounded"
-      style={{ 'fontSize': '32px' }}>photo_library</span>,
-    activeIcon: <span className="material-symbols-rounded"
-      style={{
-        'fontSize': '32px',
-        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40',
-        color: '#DB5C00'
-      }}
-    >photo_library</span>,
-    hoverIcon:
-      <span className="material-symbols-rounded"
-        style={{
-          'fontSize': '32px',
-          fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40'
-        }}>photo_library</span>,
+    icon: 'photo_library',
     path: '/dashboard/album',
     alt: 'album',
   },
   {
-    title: 'Kids’ Info',
-    icon: <span className="material-symbols-rounded"
-      style={{ 'fontSize': '32px' }}>face</span>,
-    activeIcon: <span className="material-symbols-rounded"
-      style={{
-        'fontSize': '32px',
-        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40',
-        color: '#DB5C00'
-      }}
-    >face</span>,
-    hoverIcon:
-      <span className="material-symbols-rounded"
-        style={{
-          'fontSize': '32px',
-          fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40'
-        }}>face</span>,
+    title: 'Kid\'s Info',
+    icon: 'face',
     path: '/dashboard/kids',
     alt: 'kids',
   },
   // {
   //   title: 'Settings',
-  //   icon: <span className="material-symbols-rounded"
-  //     style={{ 'fontSize': '32px' }}>settings</span>,
-  //   activeIcon:<span className="material-symbols-rounded"
-  //     style={{ 'fontSize': '32px',
-  //       fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40',
-  //       color: '#DB5C00'
-  //     }}
-  //   >settings</span>,
-  //   hoverIcon:
-  //   <span className="material-symbols-rounded"
-  //     style={{
-  //       'fontSize': '32px',
-  //       fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40'
-  //     }}>settings</span>,
+  //   icon: 'settings',
   //   path: '/dashboard/settings',
   //   alt: 'settings',
   // },
   {
     title: 'Logout',
-    icon: <span className="material-symbols-rounded"
-      style={{ 'fontSize': '32px' }}>logout</span>,
-    activeIcon: <span className="material-symbols-rounded"
-      style={{
-        'fontSize': '32px',
-        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40',
-        color: '#DB5C00'
-      }}
-    >logout</span>,
-    hoverIcon:
-      <span className="material-symbols-rounded"
-        style={{
-          'fontSize': '32px',
-          fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40'
-        }}>logout</span>,
+    icon: 'logout',
     path: '/dashboard/logout',
     alt: 'logout',
   },
   {
     title: 'Help',
-    icon: <span className="material-symbols-rounded"
-      style={{ 'fontSize': '32px' }}>help</span>,
-    activeIcon: <span className="material-symbols-rounded"
-      style={{
-        'fontSize': '32px',
-        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40',
-        color: '#DB5C00'
-      }}
-    >help</span>,
-    hoverIcon:
-      <span className="material-symbols-rounded"
-        style={{
-          'fontSize': '32px',
-          fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40'
-        }}>help</span>,
+    icon: 'help',
     path: '/dashboard/help',
     alt: 'help',
   }
