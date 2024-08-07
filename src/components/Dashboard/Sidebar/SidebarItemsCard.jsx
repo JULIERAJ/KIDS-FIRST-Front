5764f3a6ec65e667bc7686c5431011b391c111c3
Nandini Asokan <nandini.asokan92@gmail.com>
2024-08-06 20:34:32 -0400

    feature(sidebar): Fix bug introduced during previous commit

--- src/components/Dashboard/Sidebar/SidebarItemsCard.jsx
+++ src/components/Dashboard/Sidebar/SidebarItemsCard.jsx
@@ -4,9 +4,24 @@ import { NavLink } from 'react-router-dom';
 
 import styles from './Sidebar.module.css';
 
-const SidebarItemsCard = ({ title, icon, alt, path }) => {
+const SidebarItemsCard = ({ title, icon, alt, path, onClick, isActive }) => {
+  const handleClick = () => {
+    onClick(title, path);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter' || e.key === ' ') {
+      handleClick();
+    }
+  };
+
   return (
-    <NavLink to={path} className={styles.nav}>
+    <NavLink
+      to={path}
+      className={`${styles.nav} ${isActive ? styles.active : ''}`}
+      onClick={handleClick}
+      onKeyDown={handleKeyDown}
+    >
       <span alt={alt} className={'material-symbols-rounded ' + styles.icon}>
         {icon}
       </span>
@@ -18,8 +33,6 @@ const SidebarItemsCard = ({ title, icon, alt, path }) => {
 SidebarItemsCard.propTypes = {
   title: PropTypes.string.isRequired,
   icon: PropTypes.node.isRequired,
-  activeIcon: PropTypes.node.isRequired,
-  hoverIcon: PropTypes.node.isRequired,
   path: PropTypes.string,
   alt: PropTypes.string,
   onClick: PropTypes.func,
