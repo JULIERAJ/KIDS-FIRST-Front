b832fe05ccb9f9222ca0e9caaec181947e267f96
Inna Lavrova <inna.v.lavrova@gmail.com>
2024-08-09 09:06:32 -0400

    added changes in styles for landing page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -18,135 +18,135 @@ import Feature from './components/Feature/Feature';
 import Value from './components/Value/Value';
 
 export default function Home() {
-	return (
-		<>
-			<div className="banner-section" id="first-banner">
-				<div className="left-blurb">
-					<h1 className="banner-title">
+  return (
+    <>
+      <div className="banner-section" id="first-banner">
+        <div className="left-blurb">
+          <h1 className="banner-title">
 						BRIDGING CO-PARENTS AND PRIORITIZING KIDS
-					</h1>
+          </h1>
 
-					<div className="banner-text-wrapper">
-						<p>
+          <div className="banner-text-wrapper">
+            <p>
 							Our co-parenting app that helps you maintain harmony
 							in your kids&apos; lives.
-						</p>
-					</div>
+            </p>
+          </div>
 
-					<div className="banner-btn-wrapper">
-						<CustomButton
-							styles="home-signup-btn-2"
-							link="/register"
-						>
+          <div className="banner-btn-wrapper">
+            <CustomButton
+              styles="home-signup-btn-2"
+              link="/register"
+            >
 							Sign up now
-						</CustomButton>
-					</div>
-				</div>
-			</div>
+            </CustomButton>
+          </div>
+        </div>
+      </div>
 
-			<section className="site-section">
-				<div className="container">
-					<h1 className="section-title">KIDS FIRST VALUES</h1>
+      <section className="site-section">
+        <div className="container">
+          <h1 className="section-title">KIDS FIRST VALUES</h1>
 
-					<div className="values-wrapper">
-						<Value
-							title="Minimizing Conflicts"
-							text="Fostering a harmonious childhood by reducing scheduling conflicts
+          <div className="values-wrapper">
+            <Value
+              title="Minimizing Conflicts"
+              text="Fostering a harmonious childhood by reducing scheduling conflicts
 							between divorced parents"
-							img={value_1}
-						/>
+              img={value_1}
+            />
 
-						<Value
-							title="Better Child Upbringing"
-							text="Facilitating peaceful communication and coordination between parents"
-							img={value_2}
-						/>
+            <Value
+              title="Better Child Upbringing"
+              text="Facilitating peaceful communication and coordination between parents"
+              img={value_2}
+            />
 
-						<Value
-							title="Enhancing Accountability"
-							text="Improving the transparency in communication between
+            <Value
+              title="Enhancing Accountability"
+              text="Improving the transparency in communication between
 							parents and holding them accountable "
-							img={value_3}
-						/>
-					</div>
-				</div>
-			</section>
+              img={value_3}
+            />
+          </div>
+        </div>
+      </section>
 
-			<section
-				className="middle-banner-homepage"
-				id="middle-banner-homepage"
-			>
-				<div className="container">
-					<div className="middle-banner-homepage-content">
-						<h1 className="middle-banner-homepage-title">
+      <section
+        className="middle-banner-homepage"
+        id="middle-banner-homepage"
+      >
+        <div className="container">
+          <div className="middle-banner-homepage-content">
+            <h1 className="middle-banner-homepage-title">
 							KIDS HAPPINESS IS OUR PRIORITY
-						</h1>
-						<div className="middle-banner-homepage-text">
-							<p>
+            </h1>
+            <div className="middle-banner-homepage-text">
+              <p>
 								KIDS FIRST aspires to help families with simpler
 								custody scheduling by providing an interactive
 								shared calendar solution that reduces
 								misunderstandings and conflicts.
-							</p>
-						</div>
-					</div>
-				</div>
-			</section>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
 
-			<section className="site-section" id="features-prev">
-				<div className="container">
-					<div className="section-title">KIDS FIRST FEATURES</div>
+      <section className="site-section" id="features-prev">
+        <div className="container">
+          <div className="section-title">KIDS FIRST FEATURES</div>
 
-					<div className="features-wrapper">
-						<Feature
-							classModifier="feature--wide"
-							title="Dashboard"
-							img={feature_1}
-						/>
-						<Feature
-							classModifier="feature--wide"
-							title="Shared Calendar"
-							img={feature_2}
-						/>
+          <div className="features-wrapper">
+            <Feature
+              classModifier="feature--wide"
+              title="Dashboard"
+              img={feature_1}
+            />
+            <Feature
+              classModifier="feature--wide"
+              title="Shared Calendar"
+              img={feature_2}
+            />
 
-						<Feature title="Photo Album" img={feature_3} />
-						<Feature title="Kid's Info" img={feature_4} />
-						<Feature title="Mediated Messaging" img={feature_5} />
-					</div>
-				</div>
-			</section>
+            <Feature title="Photo Album" img={feature_3} />
+            <Feature title="Kid's Info" img={feature_4} />
+            <Feature title="Mediated Messaging" img={feature_5} />
+          </div>
+        </div>
+      </section>
 
-			<section className="banner-sect">
-				<div className="container">
-					<div className="banner-sect__row">
-						<div>
-							<h2 className="banner-sect__title">
+      <section className="banner-sect">
+        <div className="container">
+          <div className="banner-sect__row">
+            <div>
+              <h2 className="banner-sect__title">
 								WHY KIDS FIRST
-							</h2>
-							<div className="banner-sect__text">
-								<p>
+              </h2>
+              <div className="banner-sect__text">
+                <p>
 									We take a different approach to reducing
 									conflicts: To&nbsp;accommodate parents&apos;
 									needs with a simple and clever app design
 									that limits the need for calls or text
 									messaging.
-								</p>
-								<CustomButton
-									styles="home-signup-btn-2"
-									link="/register"
-								>
+                </p>
+                <CustomButton
+                  styles="home-signup-btn-2"
+                  link="/register"
+                >
 									Sign up now
-								</CustomButton>
-							</div>
-						</div>
-						<div>
-							<img src={mockup} alt="" />
-						</div>
-					</div>
-				</div>
-			</section>
+                </CustomButton>
+              </div>
+            </div>
+            <div>
+              <img src={mockup} alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
 
-			{/*
+      {/*
       <footer>
         <a href="#">Terms of use</a>
         <a href="#">Privacy Policy</a>
@@ -154,6 +154,6 @@ export default function Home() {
         <a href="#">About us</a>
         <a href="#">Our team</a>
       </footer> */}
-		</>
-	);
+    </>
+  );
 }
--- src/pages/Home/components/Value/Value.jsx
+++ src/pages/Home/components/Value/Value.jsx
@@ -3,23 +3,23 @@ import PropTypes from 'prop-types';
 import styles from './Value.module.css';
 
 const Value = ({ title, text, img }) => {
-	return (
-		<div className={`${styles.value} value`}>
-			<h2 className={styles['value-title']}>{title}</h2>
-			<p className={styles['value-text']}>{text}</p>
-			<img
-				src={img}
-				alt=""
-				className={styles['value-img']}
-			/>
-		</div>
-	);
+  return (
+    <div className={`${styles.value} value`}>
+      <h2 className={styles['value-title']}>{title}</h2>
+      <p className={styles['value-text']}>{text}</p>
+      <img
+        src={img}
+        alt=""
+        className={styles['value-img']}
+      />
+    </div>
+  );
 };
 
 Value.propTypes = {
-	title: PropTypes.string,
-	text: PropTypes.string,
-	img: PropTypes.string
+  title: PropTypes.string,
+  text: PropTypes.string,
+  img: PropTypes.string
 };
 
 export default Value;
