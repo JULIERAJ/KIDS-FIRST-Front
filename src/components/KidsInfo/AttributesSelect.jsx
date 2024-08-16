bb717248873da4e5b250ee22790859f41f183cdf
ElenaCherpakova <elena.cherpakova@gmail.com>
2024-08-16 02:48:09 -0400

    remove unnessary border for control

--- src/components/KidsInfo/AttributesSelect.jsx
+++ src/components/KidsInfo/AttributesSelect.jsx
@@ -81,7 +81,6 @@ const AttributesSelect = ({
           }),
           control: (base) => ({
             ...base,
-            border: '1px solid #rgba(252, 252, 252, 1)',
             borderRadius: '8px',
             padding: '8px 8px 8px 12px',
           }),
