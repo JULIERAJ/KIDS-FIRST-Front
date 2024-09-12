94f5516f8df0ea0dbd34042360a0a8c3f52c2ab5
Nandini Asokan <nandini.asokan92@gmail.com>
2024-09-11 22:41:30 -0400

    feat(ModalKid): Integrated modal into KidForm page

--- src/components/ModalKid/ModalKid.jsx
+++ src/components/ModalKid/ModalKid.jsx
@@ -1,44 +1,40 @@
-import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import React from 'react';
 import { Container } from 'react-bootstrap';
 
 import styles from '@components/shared/Modal/Modal.module.css';
 import ProfileModal from '@components/shared/Modal/ProfileModal';
 
-const KidsForm = () => {
-
+const ModalKid = ({ isModalOpen, closeModal }) => {
   const colors = ['#FFE08C', '#FDA4A6', '#CFB6EF', '#A4D1F1', '#ADE4DA'];
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  // const openModal = () => {
-  //   setIsModalOpen(true);
-  // };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-
-  };
 
   return (
     <>
       {isModalOpen && (
-        <ProfileModal onClose={closeModal}
+        <ProfileModal
+          onClose={closeModal}
           isLeftButton={false}
-          leftButtonText=""
-          title="Profile picture"
-          subtitle="Select a color for your child by clicking a circle below"
-          rightButtonText="Save">
+          leftButtonText=''
+          title='Profile picture'
+          subtitle='Select a color for your child by clicking a circle below'
+          rightButtonText='Save'
+        >
           <Container className={styles['modal-circles-container']}>
             <Container className={styles['circles-left']}>
-              <div className={styles['big-circle']}><span className={styles['tiny-circle']}></span></div>
+              <div className={styles['big-circle']}>
+                <span className={styles['tiny-circle']}></span>
+              </div>
             </Container>
 
             <Container className={styles['circles-right']}>
               {colors.map((color, index) => (
-                <div key={index}
+                <div
+                  key={index}
                   className={styles['small-circle']}
                   style={{ backgroundColor: color }}
-                ></div>))}
+                ></div>
+              ))}
             </Container>
           </Container>
         </ProfileModal>
@@ -47,4 +43,9 @@ const KidsForm = () => {
   );
 };
 
-export default KidsForm;
+ModalKid.propTypes = {
+  isModalOpen: PropTypes.func.boolean,
+  closeModal: PropTypes.func.isRequired,
+};
+
+export default ModalKid;
