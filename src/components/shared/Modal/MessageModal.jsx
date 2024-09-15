82a2a8b69486ce1573ed2fe9fd95779fad43853e
Nandini Asokan <nandini.asokan92@gmail.com>
2024-09-15 14:31:35 -0400

    feat(message-modal): fix svg import for done icon

--- src/components/shared/Modal/MessageModal.jsx
+++ src/components/shared/Modal/MessageModal.jsx
@@ -5,25 +5,29 @@ import { Container } from 'react-bootstrap';
 import { CustomButton } from '@components/shared/ui/Button/CustomButton';
 
 import icon from '@media/icons/close.svg';
-import iconRight from '@media/icons/doneOrange.svg';
+import { ReactComponent as IconRight } from '@media/icons/doneOrange.svg';
 
 import styles from './Modal.module.css';
 
 const MessageModal = ({ message, onClose }) => {
   return (
-    <Container
-      className={styles['modal-overlay']}>
-      <Container
-        className={styles['modal-container']}>
+    <Container className={styles['modal-overlay']}>
+      <Container className={styles['modal-container']}>
         <Container className={styles['modal-content']}>
           <p className={styles['modal-message']}>{message}</p>
-        </Container >
-        <Container className={styles['modal-buttons']}>
-          <CustomButton className={`secondary-light ${styles['modal-button']} ${styles['modal-message-button']}`}
-            onClick={onClose}
-            iconRight={iconRight}>Done</CustomButton>
         </Container>
-        <button className={styles['modal-close-button']} onClick={onClose}><img src={icon} /> </button>
+        <Container className={styles['modal-buttons']}>
+          <CustomButton
+            className={`secondary-light ${styles['modal-button']} ${styles['modal-message-button']}`}
+            onClick={onClose}
+            iconRight={<IconRight />}
+          >
+            Done
+          </CustomButton>
+        </Container>
+        <button className={styles['modal-close-button']} onClick={onClose}>
+          <img src={icon} />{' '}
+        </button>
       </Container>
     </Container>
   );
