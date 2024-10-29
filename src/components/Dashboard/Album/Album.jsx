892eb453eb7dea1b30bca5ed3a03dfb9b045de40
Swathy V <svanc2121@gmail.com>
2024-10-29 00:37:47 -0700

    fixed the container style

--- src/components/Dashboard/Album/Album.jsx
+++ src/components/Dashboard/Album/Album.jsx
@@ -1,35 +1,35 @@
 import PropTypes from 'prop-types';
+
 import { Container, Row } from 'react-bootstrap'; 
 
-import albumImage from '@media/album/albumEmpty.png'; 
+import album from '@media/icons/emptyAlbum.svg'; 
 
 import styles from './Album.module.css';
 
 const Album = ({ images }) => {
   return (
-    <div className={styles['parent-container']}>
-      <Container fluid className={styles['album-container']}>      
-        <Row>
-          <img src={images.albumImage} alt='albumImage' className={styles['album-image']} />
-        </Row>
-        <Row>
-          <p className={styles['album-text']}>Images sent in messages are saved here!</p>
-        </Row>
-      </Container>
-    </div>
+    
+    <Container fluid className={styles['album-container']}>      
+      <Row className={styles['album-image']}>
+        <img src={images.album} alt='albumImage' />
+      </Row>
+      <Row className={styles['album-text']}>
+        <p >Images sent in messages are saved here!</p>
+      </Row>
+    </Container>
+    
   );
 };
 
 Album.propTypes = {
   images: PropTypes.shape({
-    albumImage: PropTypes.string.isRequired,
+    album: PropTypes.string.isRequired,
   }),
 };
 
-// Default props if no images are passed
 Album.defaultProps = {
   images: {
-    albumImage,
+    album,
   },
 };
 
